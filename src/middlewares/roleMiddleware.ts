1bed4cfe2682bc141e59d88bf235433bf6421b9b
irga diputra <irgajuliand@gmail.com>
2025-06-19 21:01:33 +0700

    fix middleware

--- src/middlewares/roleMiddleware.ts
+++ src/middlewares/roleMiddleware.ts
@@ -1,7 +1,11 @@
-import { Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { UserRole } from '@prisma/client';
-import { AuthRequest } from './authMiddleware';
+import { IUserPayload } from '../interfaces/IUserPayload';
 
+// Custom interface untuk request
+export interface AuthRequest extends Request {
+  user?: IUserPayload;
+}
 export const requireRole = (roles: UserRole[]) => {
   return (req: AuthRequest, res: Response, next: NextFunction): void => {
     if (!req.user) {
--- src/routers/super.admin.router.ts
+++ src/routers/super.admin.router.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import { getAllUsersController, createStoreAdminController, assignStoreAdminController, deleteStoreAdminController, updateStoreAdminController } from "../controllers/super.admin.controller";
 import { requireRole } from "../middlewares/roleMiddleware";
-import { authMiddleware } from "../middlewares/authMiddleware";
+import { authOnlyMiddleware } from '../middlewares/authOnlyMiddleware';
 import { UserRole } from "@prisma/client";
 
 const router = express.Router();
-router.get("/users", authMiddleware, requireRole([UserRole.SUPER_ADMIN]), getAllUsersController);
-router.post("/store-admins", authMiddleware, requireRole([UserRole.SUPER_ADMIN]), createStoreAdminController);
-router.post("/stores/:storeId/admins", authMiddleware, requireRole([UserRole.SUPER_ADMIN]), assignStoreAdminController);
-router.delete("/store-admins/:userId", authMiddleware, requireRole([UserRole.SUPER_ADMIN]), deleteStoreAdminController);
-router.put("/store-admins/:userId", authMiddleware, requireRole([UserRole.SUPER_ADMIN]), updateStoreAdminController);
+router.get("/users", authOnlyMiddleware, requireRole([UserRole.SUPER_ADMIN]), getAllUsersController);
+router.post("/store-admins", authOnlyMiddleware, requireRole([UserRole.SUPER_ADMIN]), createStoreAdminController);
+router.post("/stores/:storeId/admins", authOnlyMiddleware, requireRole([UserRole.SUPER_ADMIN]), assignStoreAdminController);
+router.delete("/store-admins/:userId", authOnlyMiddleware, requireRole([UserRole.SUPER_ADMIN]), deleteStoreAdminController);
+router.put("/store-admins/:userId", authOnlyMiddleware, requireRole([UserRole.SUPER_ADMIN]), updateStoreAdminController);
 
 export default router;
