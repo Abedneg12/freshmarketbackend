bb0bdfe303ba003a49a8341e6904d618b661ccf8
Abedneg12 <yosua12.abednego@gmail.com>
2025-07-17 16:16:32 +0700

    feat: Add authentication middleware to address routes

--- src/routers/address.router.ts
+++ src/routers/address.router.ts
@@ -7,12 +7,15 @@ import {
   deleteAddressController,
 } from "../controllers/address.controller";
 import { validateBody } from "../middlewares/validationMiddleware";
+import { authOnlyMiddleware } from "../middlewares/authOnlyMiddleware";
 import {
   addressSchema,
   updateAddressSchema,
 } from "../validations/address.validation";
 
 const router = Router();
+
+router.use(authOnlyMiddleware); 
 
 router.get("/", getAllAddressesController);
 router.post("/", validateBody(addressSchema), createAddressController);
