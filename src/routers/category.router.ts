99e9768586d7fcccc455ecba2f30cb6e66f95559
irga diputra <irgajuliand@gmail.com>
2025-07-20 17:28:52 +0700

    feat: Refactor discount handling and validation, consolidate discount creation logic, and enhance product validation

--- src/routers/category.router.ts
+++ src/routers/category.router.ts
@@ -1,7 +1,12 @@
 import express from 'express';
-import { createCategoryController,deleteCategoryController,getAllCategoriesController,getCategoryByIdController,updateCategoryController } from '../controllers/category.controller';
+import { createCategoryController, deleteCategoryController, getAllCategoriesController, getCategoryByIdController, updateCategoryController } from '../controllers/category.controller';
+import { authOnlyMiddleware } from '../middlewares/authOnlyMiddleware';
+import { UserRole } from '@prisma/client';
+import { requireRole } from '../middlewares/roleMiddleware';
 
 const router = express.Router();
+
+router.use(authOnlyMiddleware, requireRole([UserRole.SUPER_ADMIN]));
 
 router.get("/", getAllCategoriesController);
 router.get("/:categoryId", getCategoryByIdController);
--- src/routers/product.router.ts
+++ src/routers/product.router.ts
@@ -1,13 +1,18 @@
 import { createProductController, updateProductController, deleteProductController, getAllProductsController, getProductByIdController } from "../controllers/product.controller";
+import { validateBody } from "../middlewares/validationMiddleware";
 import { Multer } from "../utils/multer";
 import express from 'express';
+import { productCreateSchema, productUpdateSchema } from "../validations/product.validation";
+import { authOnlyMiddleware } from "../middlewares/authOnlyMiddleware";
+import { requireRole } from "../middlewares/roleMiddleware";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 const upload = Multer("memoryStorage", "product-", "products");
 
-router.post("/", upload.array("images"), createProductController);
-router.put("/:productId", upload.array("images"), updateProductController);
-router.delete("/:productId", deleteProductController);
+router.post("/", validateBody(productCreateSchema), authOnlyMiddleware, requireRole([UserRole.SUPER_ADMIN]), upload.array("images"), createProductController);
+router.put("/:productId", validateBody(productUpdateSchema), authOnlyMiddleware, requireRole([UserRole.SUPER_ADMIN]), upload.array("images"), updateProductController);
+router.delete("/:productId", authOnlyMiddleware, requireRole([UserRole.SUPER_ADMIN]), deleteProductController);
 router.get("/", getAllProductsController);
 router.get("/:productId", getProductByIdController);
 
